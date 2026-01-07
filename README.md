54045249284264845128b23f341a5753dce2d4da
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2026-01-07 21:11:00 +0530

    add axios code for deployment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Vasitum interview-scheduler Assignment
+
+## b:https://interview-scheduler-usx9.onrender.com
+
+##
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const slotRoutes = require("./src/routes/slotRoutes");
 const userRoutes = require("./src/routes/userRoutes");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 app.use(cors());
